01990f0b698cb15002a5790b46024978f16023c9
Jordan Porter <silentsummitsoftware@gmail.com>
2021-06-15 13:03:48 -0700

    initial commit

---
+++ scripts/popup-agent.js
@@ -0,0 +1,4 @@
+
+;window.NREUM||(NREUM={});NREUM.init={distributed_tracing:{enabled:true},privacy:{cookies_enabled:true}};
+;NREUM.loader_config={accountID:"550352",trustKey:"1",agentID:"34690206",licenseKey:"2fec6ab188",applicationID:"34690206"}
+;NREUM.info={beacon:"staging-bam-cell.nr-data.net",errorBeacon:"staging-bam-cell.nr-data.net",licenseKey:"2fec6ab188",applicationID:"34690206",sa:1}
